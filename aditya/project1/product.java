c7911e096e4f18ce0b1b971e6e239d1a77f6d151
aaditya2708 <96545022+aaditya2708@users.noreply.github.com>
2023-05-15 17:32:38 +0530

    adi

--- aditya/project1/product.java
+++ aditya/project1/product.java
@@ -4,12 +4,17 @@ import java.util.Scanner;
 
 public class product {
     public static void main(String[] args) {
-        System.out.println("enter product name:");
+        // System.out.println("enter product name:");
         userData ud = new userData();
         Scanner s = new Scanner(System.in);
-        ud.setproductName(s.next());
+        String name[] = new String[10];
+        for (int i = 0; i < 3; i++) {
+            System.out.println("enter name:");
+            name[i] = s.next();
+        }
+        ud.setproductName(name);
         System.out.println("enter quantity");
         ud.setquantity(s.nextInt());
-        
+
     }
 }
